65929630bd69d82b03b35928832f165b07271160
ShizVizor <vladimir.vasilenko.0303@mail.ru>
2025-04-04 19:15:47 +0700

    Permission odd class deleted

--- backend/foodgramm/api/permissions.py
+++ backend/foodgramm/api/permissions.py
@@ -5,8 +5,3 @@ class IsAuthorOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.method in SAFE_METHODS or obj.author == request.user
-
-
-class IsAuthorOrAdmin(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.author == request.user or request.user.is_staff
